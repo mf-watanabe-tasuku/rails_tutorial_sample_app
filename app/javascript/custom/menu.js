55b41bd287ebbbc220b75c0f7638aff4c649f9a6
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2023-10-26 23:56:41 +0900

    Finish user login and test

---
+++ app/javascript/custom/menu.js
@@ -0,0 +1,17 @@
+// メニュー操作
+
+// トグルリスナーを追加する
+function addToggleListener(selected_id, menu_id, toggle_class) {
+  let selected_element = document.querySelector(`#${selected_id}`);
+  selected_element.addEventListener("click", function(e) {
+    e.preventDefault();
+    let menu = document.querySelector(`#${menu_id}`);
+    menu.classList.toggle(toggle_class);
+  });
+}
+
+// クリックをリッスンするトグルリスナーを追加する
+document.addEventListener("turbo:load", function() {
+  addToggleListener("hamburger", "navbar-menu", "collapse");
+  addToggleListener("account", "dropdown-menu", "active");
+});
