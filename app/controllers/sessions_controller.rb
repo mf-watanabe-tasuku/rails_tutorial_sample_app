a94630fa3b7c757a849dc531904129ebb7ce4cce
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2023-10-28 21:25:24 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
@@ -6,16 +7,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = "Invalid email/password combination"
+      flash.now[:danger] = 'Invalid email/password combination'
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 end
--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -3,7 +3,7 @@
 // トグルリスナーを追加する
 function addToggleListener(selected_id, menu_id, toggle_class) {
   let selected_element = document.querySelector(`#${selected_id}`);
-  selected_element.addEventListener("click", function(e) {
+  selected_element?.addEventListener("click", function(e) {
     e.preventDefault();
     let menu = document.querySelector(`#${menu_id}`);
     menu.classList.toggle(toggle_class);
