09c955a1199a50ae075ff8879be72cbebc777b6c
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2023-11-02 23:50:49 +0900

    add pagination for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
 
   def index
-    @users = User.all
+    p User.all.length
+    @users = User.paginate(page: params[:page])
   end
 
   def show
